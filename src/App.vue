a94f7a9fe0108664bdf3da7e1e3de3e0e98c126d
ZhangYiBo1998 <27582180@qq.com>
2024-01-15 21:18:09 +0800

    初始化

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
-    <div>App</div>
+    <div>
+        <h1>{{ msg }}</h1>
+        <p>{{ obj.msg }}</p>
+    </div>
 </template>
 
 <script lang="ts">
+import { reactive, ref } from "vue";
+
 export default {
     name: "App",
+    components: {},
+    props:[],
     setup() {
-      
+        let msg = ref("我是App组件");
+        let obj = reactive({
+            msg: "hello world!"
+        })
+        return {
+            msg,
+            obj
+        }
     },
 };
+
 </script>
 
-<style></style>
+<style scoped>
+
+</style>
